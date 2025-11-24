6daa0bfdde12ab916dff32c7b1fd760efc8681d2
Yifan Gao <git@yfgao.com>
2025-11-24 18:01:53 +0800

    Replace mrt dependency with bgpkit-parser and refactor main function to utilize the new parser for processing MRT/RIB files.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
+use bgpkit_parser::{models::ElemType, BgpkitParser};
 use std::collections::HashSet;
-use std::fs::File;
 use std::path::PathBuf;
 use structopt::StructOpt;
-use try_match::try_match;
-use what_i_want::*;
-
-extern crate mrt;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "bgptools")]
@@ -17,16 +13,6 @@ struct Opts {
     asns: Vec<String>,
 }
 
-macro_rules! match_or_continue {
-    ($in:expr, $(|)? $($p:pat_param)|+ $(if $guard:expr)? => $out:expr) => {
-        unwrap_or_continue!(try_match!($in, $($p)|+ $(if $guard)? => $out))
-    };
-
-    ($in:expr, $(|)? $($p:pat_param)|+ $(if $guard:expr)?) => {
-        unwrap_or_continue!(try_match!($in, $($p)|+ $(if $guard)?))
-    };
-}
-
 fn main() {
     let opts: Opts = Opts::from_args();
     let asn_list: HashSet<u32> = opts
@@ -34,29 +20,26 @@ fn main() {
         .into_iter()
         .map(|x| x.parse::<u32>().expect("args(ASN) must be a number!"))
         .collect();
-    let file = File::open(&opts.mrt_file).unwrap();
-    let entries = mrt::read_file_complete(file).unwrap();
-    for entry in &entries {
-        match_or_continue!(&entry.mrt_header.mrt_type, mrt::MrtType::TABLE_DUMP_V2);
-        let (header, entries) = match_or_continue!(
-            &entry.message,
-            mrt::MrtMessage::RIB_IPV4_UNICAST { header, entries }
-                | mrt::MrtMessage::RIB_IPV6_UNICAST { header, entries } => (header, entries)
-        );
-        for e in entries {
-            for a in &e.bgp_attributes {
-                let segments = match_or_continue!(
-                    &a.value,
-                    mrt::BgpAttributeValue::AS_PATH { segments } => segments
-                );
-                for s in segments {
-                    match_or_continue!(&s.segment_type, mrt::SegmentType::AS_SEQUENCE);
-                    let asn = unwrap_or_continue!(s.asns.last());
-                    if asn_list.contains(asn) {
-                        println!("{}/{}", header.prefix, header.prefix_length);
-                    }
-                }
-            }
+
+    let rib_path = opts.mrt_file.to_string_lossy().into_owned();
+    let parser =
+        BgpkitParser::new(rib_path.as_str()).expect("failed to open MRT/RIB file with bgpkit");
+
+    for elem in parser.into_elem_iter() {
+        if !matches!(elem.elem_type, ElemType::ANNOUNCE) {
+            continue;
+        }
+
+        let origins = match &elem.origin_asns {
+            Some(origins) => origins,
+            None => continue,
+        };
+
+        if origins
+            .iter()
+            .any(|asn| asn_list.contains(&asn.to_u32()))
+        {
+            println!("{}", elem.prefix);
         }
     }
 }
